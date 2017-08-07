3473f526af0e6d40d4aee426d796532e6c50462d
Jaideep Sundaram <sundaram.fitstack@gmail.com>
2017-08-06 21:58:23 -0400

    initial check-in

---
+++ bin/run.sh
@@ -0,0 +1,1 @@
+rm -rf /tmp/umlet-test && mkdir /tmp/umlet-test && perl util/umlet_class_diagram_to_javascript_api.pl --infile ~/Downloads/Analysis_Class_Diagram_2017-07-14.uxf --outdir /tmp/umlet-test
